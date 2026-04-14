7fddf050e589c1409960ad14d3b680d1769269d7
lucio.souza <lucio.souza@el.com.br>
2026-04-13 22:02:31 -0300

    Criando funcionalidades imagens

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -6,7 +6,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pinterest.db'
 app.config['SECRET_KEY'] = '5f4152dba8d9c3f0d089ac4aa2ce1372'
-
+app.config['UPLOAD_FOLDER'] = 'static/fotos_posts'
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
